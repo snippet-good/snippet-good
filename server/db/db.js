eda49ef5071939962b054f4dbd98cd1371af62c1
Steven Levine <slevine111@gmail.com>
2019-05-22 19:03:53 -0400

    changed process.env.URL to process.env.DATABASE_URL

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
 module.exports = new Sequelize(
-  process.env.URL || 'postgres://localhost/snippet_db',
+  process.env.DATABASE_URL || 'postgres://localhost/snippet_db',
   {
     logging: false
   }
