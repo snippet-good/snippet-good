c3f3f97770b47a0747d97572d474c2144d6a78bc
dm1031 <danmdrums@gmail.com>
2019-05-23 20:18:24 -0400

    changed initial state to object

--- client/redux/store/user-reducer.js
+++ client/redux/store/user-reducer.js
@@ -1,6 +1,6 @@
 import { GET_USER } from './actions'
 
-export default (state = [], action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case GET_USER:
       state = action.user
