1078c6fd36e1eb14f6a35f89a136d23960739d22
Steven Levine <slevine111@gmail.com>
2019-05-29 09:04:38 -0400

    updated GET /api/stretches

--- server/api/stretches.js
+++ server/api/stretches.js
@@ -1,11 +1,27 @@
 const router = require('express').Router()
 const {
-  models: { Stretch, Category }
+  models: { Stretch, Category, User }
 } = require('../db/index')
 
 // GET, retrieves all stretches from the database
 router.get('/', (req, res, next) => {
-  Stretch.findAll({ include: [Category] })
+  Stretch.findAll({
+    include: [
+      Category,
+      { model: User, as: 'author', attributes: ['firstName', 'lastName'] }
+    ]
+  })
+    .then(stretches => {
+      return stretches.map(stretch => {
+        const data = stretch.get()
+        const { category, author, ...stretchFields } = data
+        return {
+          ...stretchFields,
+          categoryName: category.name,
+          autherName: `${author.firstName} ${author.lastName}`
+        }
+      })
+    })
     .then(stretches => res.json(stretches))
     .catch(next)
 })
