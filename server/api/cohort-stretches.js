0e2461c94ff9352c83f2d3d7f000b8c9d00cb3d7
Steven Levine <slevine111@gmail.com>
2019-05-29 15:38:52 -0400

    Updated GET /api/cohort-stretches with includes

--- server/api/cohort-stretches.js
+++ server/api/cohort-stretches.js
@@ -1,11 +1,39 @@
 const router = require('express').Router()
 const {
-  models: { CohortStretch }
+  models: { CohortStretch, Cohort, User, CohortUser }
 } = require('../db/index')
 
 // GET, retrieves all cohort stretches from the database
 router.get('/', (req, res, next) => {
-  CohortStretch.findAll()
+   CohortStretch.findAll({
+      include: [
+        {
+          model: Cohort,
+          attributes: ['name'],
+          include: [
+            {
+              model: CohortUser,
+              include: [
+                { model: User, where: { isAdmin: true }, attributes: [] }
+              ]
+            }
+          ]
+        }
+      ]
+    })
+  })
+  .then(cohortStretches => {
+    return cohortStretches.map(cohortStretch => {
+      const values = cohortStretch.get()
+      const { cohort, ...cohortStretchesFields } = values
+      const cohortValues = cohort.get()
+      const { cohortusers, name } = cohortValues
+      return {
+        ...cohortStretchesFields,
+        cohortName: name,
+        adminIds: cohortusers.map(cu => cu.userId)
+      }
+    })
     .then(cohortStretches => res.json(cohortStretches))
     .catch(next)
 })
