efadb9f3723faa15c3cba466c631e801b02100ed
Steven Levine <slevine111@gmail.com>
2019-06-05 18:26:52 -0400

    Added handleChange as prop to be passed in CodeEditor

--- client/Components/CodeEditor/configeditor.js
+++ client/Components/CodeEditor/configeditor.js
@@ -1,5 +1,30 @@
-const configEditor = function(editor, editorSession, editorTheme) {
-  editor.setTheme(`ace/theme/${editorTheme}`)
+const addClosingCurlyBracket = (editor, editorSession) => {
+  const cursorPosition = editor.getCursorPosition()
+  const enteredCharacterPosition = {
+    start: { ...cursorPosition },
+    end: { ...cursorPosition, column: cursorPosition.column + 1 }
+  }
+  const { end } = enteredCharacterPosition
+  const nextCharacterPosition = {
+    start: end,
+    end: { ...end, column: end.column + 1 }
+  }
+  if (
+    editorSession.getTextRange(enteredCharacterPosition) === '{' &&
+    editorSession.getTextRange(nextCharacterPosition) !== '}'
+  ) {
+    editorSession.insert(nextCharacterPosition.start, '}')
+  }
+}
+
+const configEditor = function(
+  editor,
+  editorSession,
+  editorTheme,
+  themeFromState,
+  handleCodeChange
+) {
+  if (themeFromState) editor.setTheme(`ace/theme/${editorTheme}`)
   editorSession.setMode('ace/mode/javascript')
   editor.setShowPrintMargin(false)
   editor.setOptions({ minLines: 15 })
@@ -12,23 +37,12 @@ const configEditor = function(editor, editorSession, editorTheme) {
   })
 
   editorSession.on('change', () => {
-    const cursorPosition = editor.getCursorPosition()
-    const enteredCharacterPosition = {
-      start: { ...cursorPosition },
-      end: { ...cursorPosition, column: cursorPosition.column + 1 }
+    addClosingCurlyBracket(editor, editorSession)
+    if (typeof handleCodeChange === 'function') {
+      handleCodeChange(editorSession.getValue())
+    } else {
+      this.setState({ code: editorSession.getValue() })
     }
-    const { end } = enteredCharacterPosition
-    const nextCharacterPosition = {
-      start: end,
-      end: { ...end, column: end.column + 1 }
-    }
-    if (
-      editorSession.getTextRange(enteredCharacterPosition) === '{' &&
-      editorSession.getTextRange(nextCharacterPosition) !== '}'
-    ) {
-      editorSession.insert(nextCharacterPosition.start, '}')
-    }
-    this.setState({ code: editorSession.getValue() })
   })
 }
 
