05223ed5607be332325fb12b56bc636dec619939
Steven Levine <slevine111@gmail.com>
2019-06-22 01:21:29 -0400

    made tables in single cohrot stretches collaplse

--- client/Components/SingleCohort/SingleCohortStretchTables.js
+++ client/Components/SingleCohort/SingleCohortStretchTables.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { checkIfAllDataExists } from '../../utilityfunctions'
 
+import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import ListItem from '@material-ui/core/ListItem'
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+import Collapse from '@material-ui/core/Collapse'
 import SingleTable from './SingleTable'
 import { formatCohortStretch } from '../StudentHomeView/helperfunctions'
 
@@ -54,18 +59,36 @@ const SingleCohortStretchTables = ({ cohort, cohortStretches, stretches }) => {
   return (
     <div>
       {tables.map(table => {
+        let [expandedTable, setExpandTable] = useState(true)
         const { key, title } = table
         return (
           <div key={key}>
-            <Typography variant="h6" id="tableTitle">
-              {title}
-            </Typography>
-            <SingleTable
-              data={groupedStretches[key]}
-              dbColumnNames={dbColumnNames[key]}
-              tableColumnNames={tableColumnNames[key]}
-              status={key}
-            />
+            <ListItem
+              button
+              onClick={() => {
+                console.log(expandedTable)
+                setExpandTable(!expandedTable)
+              }}
+            >
+              <Grid container>
+                <Grid item xs={6}>
+                  <Typography variant="h6" id="tableTitle">
+                    {title}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6} style={{ textAlign: 'right' }}>
+                  {expandedTable ? <ExpandLess /> : <ExpandMore />}
+                </Grid>
+              </Grid>
+            </ListItem>
+            <Collapse in={expandedTable} timeout="auto" unmountOnExit>
+              <SingleTable
+                data={groupedStretches[key]}
+                dbColumnNames={dbColumnNames[key]}
+                tableColumnNames={tableColumnNames[key]}
+                status={key}
+              />
+            </Collapse>
           </div>
         )
       })}
